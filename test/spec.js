1b23e5ffb09c56497e43d9c6b7ac97f7cf620b69
Andy Perlitch <andyperlitch@gmail.com>
2013-04-23 10:38:04 -0700

    switched to options object for 2nd param in timeSince, added unixUptime

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var bormat = require('../');
-describe("a boring format util", function() {
+describe("a boring format util: ", function() {
     
     describe("a comma-group number formatter", function() {
         it("should format number strings with commas every three places", function(){
@@ -24,13 +24,18 @@ describe("a boring format util", function() {
         it("should be able to take a compare date in lieu of assuming the current timestamp", function() {
             var compare = +new Date() - 172800000; // 2 days from now
             var timestamp = compare - 3602000; // 1 hour and 2 seconds before compare
-            assert.equal( bormat.timeSince(timestamp, compare), "1 hour, 2 seconds", "timeSince did not work with a provided compare date");
+            assert.equal( bormat.timeSince(timestamp, { compareDate: compare }), "1 hour, 2 seconds", "timeSince did not work with a provided compare date");
         });
         
         it("should be able to specify a max unit size", function() {
             var timestamp = +new Date() - 2419200000 - 604800000;
             assert.equal( bormat.timeSince(timestamp), "1 month, 1 week", "timeSince did not work with control test for max unit");
-            assert.equal( bormat.timeSince(timestamp, undefined, undefined, "day"), "35 days", "timeSince did not work with a max unit specified");
+            assert.equal( bormat.timeSince(timestamp, { maxUnit: "day" }), "35 days", "timeSince did not work with a max unit specified");
+        });
+        
+        it("should be able to output unix uptime format", function() {
+            var timestamp = +new Date() - 2419200000 - 604800000 - 3720000; // 35 days, 1 hour, 2 minutes
+            assert.equal( bormat.timeSince(timestamp, {unixUptime: true}), "35 days, 1:02", "timeSince did not format unix uptime correctly");
         });
         
     });
